2925d323a55bb324969e7b87adb490e38968e160
lobasfs <lobasfs@mail.ru>
2026-02-02 16:38:56 +0900

    Update main.tf

--- infrastructure/terraform/modules/monitoring/main.tf
+++ infrastructure/terraform/modules/monitoring/main.tf
@@ -1,1 +1,18 @@
+# Monitoring Module
 
+resource "yandex_monitoring_dashboard" "main" {
+  name        = "${var.project_name}-dashboard"
+  description = "Main monitoring dashboard"
+
+  parametrization {
+    parameters {
+      id    = "cluster"
+      title = "Cluster"
+      values = [yandex_kubernetes_cluster.main.id]
+    }
+  }
+
+  labels = {
+    environment = var.environment
+  }
+}
