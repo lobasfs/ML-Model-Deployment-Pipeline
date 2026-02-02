d106621548bc3ebca5cd448977ab515cbcbbf58c
lobasfs <lobasfs@mail.ru>
2026-02-02 16:40:45 +0900

    Update main.tf

--- infrastructure/terraform/main.tf
+++ infrastructure/terraform/main.tf
@@ -1,1 +1,28 @@
+terraform {
+  required_version = ">= 1.0"
 
+  required_providers {
+    yandex = {
+      source  = "yandex-cloud/yandex"
+      version = "~> 0.100"
+    }
+  }
+
+  # Remote state в Object Storage
+  backend "s3" {
+    endpoint   = "storage.yandexcloud.net"
+    bucket     = "ml-terraform-state"
+    region     = "ru-central1"
+    key        = "terraform.tfstate"
+
+    skip_region_validation      = true
+    skip_credentials_validation = true
+  }
+}
+
+provider "yandex" {
+  token     = var.yandex_token
+  cloud_id  = var.cloud_id
+  folder_id = var.folder_id
+  zone      = var.zone
+}
