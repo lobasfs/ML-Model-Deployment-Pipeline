37cb4ef8e208badfe1d9482d24d5c6f95ee978de
lobasfs <lobasfs@mail.ru>
2026-02-02 16:42:33 +0900

    Update variables.tf

--- infrastructure/terraform/variables.tf
+++ infrastructure/terraform/variables.tf
@@ -1,1 +1,63 @@
+variable "yandex_token" {
+  description = "Yandex Cloud OAuth token"
+  type        = string
+  sensitive   = true
+}
 
+variable "cloud_id" {
+  description = "Yandex Cloud ID"
+  type        = string
+}
+
+variable "folder_id" {
+  description = "Yandex Cloud Folder ID"
+  type        = string
+}
+
+variable "zone" {
+  description = "Yandex Cloud zone"
+  type        = string
+  default     = "ru-central1-a"
+}
+
+variable "environment" {
+  description = "Environment name"
+  type        = string
+  default     = "production"
+}
+
+variable "project_name" {
+  description = "Project name"
+  type        = string
+  default     = "ml-deployment"
+}
+
+variable "k8s_version" {
+  description = "Kubernetes version"
+  type        = string
+  default     = "1.28"
+}
+
+variable "node_cpu_count" {
+  description = "Number of CPUs per node"
+  type        = number
+  default     = 4
+}
+
+variable "node_memory_gb" {
+  description = "Memory in GB per node"
+  type        = number
+  default     = 8
+}
+
+variable "node_count" {
+  description = "Initial number of nodes"
+  type        = number
+  default     = 3
+}
+
+variable "max_node_count" {
+  description = "Maximum number of nodes for autoscaling"
+  type        = number
+  default     = 10
+}
