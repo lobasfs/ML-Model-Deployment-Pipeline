581c09269333039934c3dd8fcd1c677160479a83
lobasfs <lobasfs@mail.ru>
2026-02-02 16:41:47 +0900

    Update outputs.tf

--- infrastructure/terraform/outputs.tf
+++ infrastructure/terraform/outputs.tf
@@ -1,1 +1,19 @@
+output "cluster_endpoint" {
+  description = "Kubernetes cluster endpoint"
+  value       = module.kubernetes.cluster_endpoint
+}
 
+output "models_bucket" {
+  description = "S3 bucket for models"
+  value       = module.storage.models_bucket
+}
+
+output "data_bucket" {
+  description = "S3 bucket for data (DVC)"
+  value       = module.storage.data_bucket
+}
+
+output "kubeconfig_command" {
+  description = "Command to get kubeconfig"
+  value       = "yandex managed-kubernetes cluster get-credentials ${module.kubernetes.cluster_id} --external"
+}
